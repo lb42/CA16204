e4f2f3ca96566565302717addbfe7b26ee84ea73
lou burnard <lou.burnard@retired.ox.ac.uk>
2020-09-06 20:31:03 +0100

    tidying bookLists

--- bookLists/SQL/genreList.sql
+++
@@ -1,3 +0,0 @@
-select genres.genre_name,genres_titles.title_id from genres, genres_titles where genres_titles.genre_id = genres.genre_id order by genres_titles.title_id;
-
- select eltec_titles.eltec_id,  genres_titles.title_id , genres.genre_name from eltec_titles, genres, genres_titles where genres_titles.title_id = eltec_titles.title_id and genres_titles.genre_id = genres.genre_id order by genres.genre_name;
